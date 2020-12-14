fc9233f6cb2005fa5908209c9f077f37431cf0b5
sh960440 <54958110+sh960440@users.noreply.github.com>
2020-12-14 15:53:03 +0800

    Player movement

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Controls")]
+    public Joystick joystick;
+    public float joystickHorizontalSensitivity;
+    public float joystickVerticalSensitivity;
+    public float horizontalForce;
+    public float verticalForce;
+
+    [Header("Platform Detection")]
+    public bool isGrounded;
+    public bool isJumping;
+    public Transform spawnPoint;
+
+    public int lives;
+
+    private Animator m_animator;
+    private SpriteRenderer m_spriteRenderer;
+    private Rigidbody2D m_rigidBody2D;
+
+    void Start()
+    {
+        lives = 5;
+
+        m_rigidBody2D = GetComponent<Rigidbody2D>();
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+        m_animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        _Move();
+    }
+
+    void _Move()
+    {
+        //if (isGrounded)
+        //{
+            if (!isJumping)
+            {
+                if (joystick.Horizontal > joystickHorizontalSensitivity)
+                {
+                    // move right
+                    m_rigidBody2D.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+                    //CreateDustTrail();
+
+                    //m_animator.SetInteger("AnimState", (int)PlayerAnimationType.RUN);
+                }
+                else if (joystick.Horizontal < -joystickHorizontalSensitivity)
+                {
+                    // move left
+                    m_rigidBody2D.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
+                    transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+
+                    //CreateDustTrail();
+
+                    //m_animator.SetInteger("AnimState", (int)PlayerAnimationType.RUN);
+                }
+                else
+                {
+                    //m_animator.SetInteger("AnimState", (int)PlayerAnimationType.IDLE);
+                }
+            }
+
+            if ((joystick.Vertical > joystickVerticalSensitivity) && (!isJumping))
+            {
+                // jump
+                m_rigidBody2D.AddForce(Vector2.up * verticalForce);
+                //m_animator.SetInteger("AnimState", (int) PlayerAnimationType.JUMP);
+                isJumping = true;
+
+                //sounds[(int) ImpulseSounds.JUMP].Play();
+
+                //CreateDustTrail();
+            }
+            else
+            {
+                isJumping = false;
+            }   
+        //}
+    }
+
+}
